4ac68d1c0371240b3da39f6877f87106f1838c60
romaArtemyev <golodnyye.nogi@gmail.com>
2017-11-01 02:09:50 +1000

    add background, panel to enter name and price

--- app/src/main/java/com/loftschool/moneytracker/MainActivity.java
+++ app/src/main/java/com/loftschool/moneytracker/MainActivity.java
@@ -2,37 +2,68 @@ package com.loftschool.moneytracker;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText name = findViewById(R.id.name);
+        final EditText price = findViewById(R.id.price);
+        final ImageButton add = findViewById(R.id.addBtn);
+
+        add.setEnabled(false);
+
+       name.addTextChangedListener(new TextWatcher() {
+           @Override
+           public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+           }
+
+           @Override
+           public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+           }
+
+           @Override
+           public void afterTextChanged(Editable s) {
+                if (!price.getText().toString().equals("")) add.setEnabled(true);
+           }
+       });
+
+       price.addTextChangedListener(new TextWatcher() {
+           @Override
+           public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+           }
+
+           @Override
+           public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+           }
+
+           @Override
+           public void afterTextChanged(Editable s) {
+               if (!name.getText().toString().equals("")) add.setEnabled(true);
+           }
+       });
+
+        add.setOnClickListener(this);
+
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
+    public void onClick(View v) {
+        Toast toast = Toast.makeText(getApplicationContext(),
+                "Пора покормить кота!", Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
