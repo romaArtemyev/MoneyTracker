c63398c374697718ee7896a360f4190861a07b9e
romaArtemyev <golodnyye.nogi@gmail.com>
2017-11-14 09:12:58 +1000

    FAB for adding new items.

--- app/src/main/java/com/loftschool/moneytracker/Item.java
+++ app/src/main/java/com/loftschool/moneytracker/Item.java
@@ -1,6 +1,8 @@
 package com.loftschool.moneytracker;
 
-public class Item{
+import java.io.Serializable;
+
+public class Item implements Serializable{
 
     public static final String TYPE_UNKNOWN = "unknown";
     public static final String TYPE_SPENDING = "spending";
